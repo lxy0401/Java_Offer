16568dc1ddaccec2c370124b7bff75524b0aa8cf
403824215@qq.com <403824215@qq.com>
2018-07-19 16:24:47 +0800

    根据先序中序遍历重建二叉树

---
+++ RebuildTree/TreeNode.java
@@ -0,0 +1,16 @@
+package RebuildTree;
+
+/**
+ * @author LXY
+ * @email 403824215@qq.com
+ * @date 2018/7/17 17:20
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
